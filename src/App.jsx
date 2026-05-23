9c5d721f4abc92331e01324b7773e809bdfbb956
Justin <justin@endlessaccess.org>
2026-05-22 21:33:40 -0300

    cambios permanentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Cards />
+      <Footer />
     </>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -88,3 +88,4 @@ export function Cards() {
     </section>
   );
 }
+export default Cards;
